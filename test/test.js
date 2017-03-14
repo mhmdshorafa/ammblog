3960cfc19ef33d08c72360c5dec7bcbc392c0136
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-03-14 13:10:29 +0200

    add database Files and test

--- test/test.js
+++ test/test.js
@@ -13,6 +13,7 @@ test('endpoint test | POST /subscribe | empty payload -> 400 Bad Request', t => 
   return server.inject(request)
     .then(response => {
       t.is(response.statusCode, 400, 'status code is 400');
+        process.exit(1);
       t.end();
     });
 });
