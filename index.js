2961fd9a854d8fb33923fe3ccf883a0c77d88fa8
abeeralshaer <abber2000g@gmail.com>
2017-03-14 21:00:28 -0500

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
-const conncrea = require('./app/dbutils/client.js');
+
 
 server.connection({
   port: process.env.PORT || 8080
@@ -14,24 +14,7 @@ server.route({
       return reply("Hello");
   }
 });
-server.route({
-  method: 'POST',
-  path: '/subscribe',
-  handler: (request, reply) => {
 
-      if (!request.payload.email) {
-          return reply({
-              result: 'failure',
-              message: 'Email address is required.'
-          }).code(400);
-      }
-  }
-});
-if (!module.parent) {
-  server.start(error => {
-      console.log(error);
-      process.exit(1);
-  });
-}
 
+  server.start(()=>{});
 module.exports = server;
--- test/test.js
+++ test/test.js
@@ -8,13 +8,34 @@ const requestDefaults = {
 };
 
 test('endpoint test | GET/ | Right Request', t => {
-    const request = Object.assign({}, requestDefaults);//this method is to re-write the same request with different parameters or payload instead of re-writting the same request a couple of times 
+    const request = Object.assign({}, requestDefaults);//this method is to re-write the same request with different parameters or payload instead of re-writting the same request a couple of times
     return server.inject(request)
         .then(response => {
             t.is(response.statusCode, 200, 'status code is 200');
 
             server.stop(t.end);
         });
+});
+test('endpoint test | GET/main | Bad Request', t => {
+    const request = Object.assign({}, requestDefaults,{
+      url:'/main'
+    });//this method is to re-write the same request with different parameters or payload instead of re-writting the same request a couple of times
+    return server.inject(request)
+        .then(response => {
+            t.is(response.statusCode, 404, 'status code is 400');
+
+            server.stop(t.end);
+        });
+});
+test('endpoint test | GET/articles | return html page with articles', t => {
+
+});
+
+test('controlpanel test | Post/controller/addArticle | add new article to db', t => {
+
+});
+test('endpoint test | Post/controller/deleteArticle | return html page with articles', t => {
+
 });
 
 test.onFinish(() => process.exit(0));
